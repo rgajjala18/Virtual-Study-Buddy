cfe70afbe585f699978993cb14ae0dcfba07b805
Jasoniyu <jiy6krm@virginia.edu>
2020-10-01 14:29:56 -0400

    Another attempt

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,7 +111,7 @@ AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
                            'allauth.account.auth_backends.AuthenticationBackend',
                            )
 
-SITE_ID = 2
+SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 
