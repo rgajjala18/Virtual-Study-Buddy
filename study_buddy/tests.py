7a827fb3c840b94e7fb6e97b3d713d314eac86fd
Grace Brickley <gracebrickley@Tamaras-Air.zyxel.com>
2020-11-01 22:00:17 -0500

    forms tests

--- study_buddy/tests.py
+++ study_buddy/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from .models import Student
+from .models import Course
 from django.contrib.auth import get_user_model
-
-
+from study_buddy.forms import UserForm, ProfileForm
 
 class UsersManagersTests(TestCase):
 
@@ -33,3 +33,25 @@ class ModelsTests(TestCase):
         s = self.create_student()
         self.assertTrue(isinstance(s, Student))
         self.assertEqual(s.__str__(), "John Smith")
+
+    def create_course(self, letters="CS", numbers="3240"):
+        return Course.objects.create(prefix=letters, number=numbers)
+
+    def test_course_name(self):
+        t = self.create_course()
+        self.assertTrue(isinstance(t, Course))
+        self.assertEqual(t.__str__(), "CS 3240")
+
+class FormsTests(TestCase):
+
+    def test_valid_form(self):
+        s = Student.objects.create(email='johnsmith@gmail.com')
+        data = {'email': s.email,}
+        form = UserForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_valid_form_pt2(self):
+        s = Student.objects.create(firstName='john', lastName='Smith', computingID='jds4fk', phone='8044325324', zoomID='94392843', graduationYear='2022', bio='Hi!', )
+        data = {'firstName': s.firstName, 'lastName': s.lastName, 'computingID': s.computingID, 'phone': s.phone, 'zoomID': s.zoomID, 'graduationYear': s.graduationYear, 'bio': s.bio,}
+        form = ProfileForm(data=data)
+        self.assertTrue(form.is_valid())
